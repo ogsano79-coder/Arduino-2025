e994d6614d7220e802404655c6d3dc06cb0276e1
Yiğit Oğuz <ogsano79@gmail.com>
2025-12-19 10:43:32 +0300

    Add files via upload

---
+++ sirayla_yanan_ledler_kod/s_rayla_yanan_ledler_kod.ino
@@ -0,0 +1,43 @@
+int on=10,onbir=11,oniki=12,onuc=13;
+void setup()
+{
+  pinMode(on ,OUTPUT);
+  pinMode(onbir ,OUTPUT);
+  pinMode(oniki ,OUTPUT);
+  pinMode(onuc ,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(on, HIGH);
+  delay(100);
+  digitalWrite(on, LOW);
+  delay(100);
+  
+  
+  digitalWrite(onbir, HIGH);
+  delay(100);
+  digitalWrite(onbir, LOW);
+  delay(100);
+  
+  digitalWrite(oniki, HIGH);
+  delay(100);
+  digitalWrite(oniki, LOW);
+  delay(100);
+  
+  
+  digitalWrite(onuc, HIGH);
+  delay(100);
+  digitalWrite(onuc, LOW);
+  delay(100);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
